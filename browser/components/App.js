d81a5803a21367f4dd530bcf83a9f91c5a07d0fc
doug.wade <doug.wade@redfin.com>
2016-04-16 11:26:34 -0700

    Fix linter errors

--- browser/components/App.js
+++ browser/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AddTodo from './AddTodo';
 import Todo from './Todo';
 import HelloWorld from './HelloWorld';
-import EventMap from './EventMap'
+import EventMap from './EventMap';
 import { connect } from 'react-redux';
 
 const mapStateToProps = (state) => {
