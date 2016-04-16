6a567cd59d5a9f496ff4d074f9608dda15c95b3d
= <kristoffer@kristofferhebert.com>
2016-04-16 11:26:27 -0700

    adding map

--- browser/components/EventMap.js
+++ browser/components/EventMap.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import {GoogleMapLoader, GoogleMap, Marker} from "react-google-maps";
-//import { getExcited } from '../actions';
 
 const mapStateToProps = (state) => {
   return {
