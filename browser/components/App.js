3109e7b4f1e11cf106f50402b1cec5965878a982
doug.wade <doug.wade@redfin.com>
2016-04-17 00:14:01 -0700

    Get roles from the db

--- browser/components/App.js
+++ browser/components/App.js
@@ -3,9 +3,18 @@ import HelloWorld from './HelloWorld';
 import EventMap from './EventMap';
 import Navigation from './Navigation';
 import { connect } from 'react-redux';
+import { updateRoles } from '../actions';
 import '../styles/app.css';
 
-let App = () => {
+let App = ({ dispatch }) => {
+  // Ideally, we'd get the data server-side and return it with the initial bundle
+  var rolesReq = new XMLHttpRequest();
+  rolesReq.addEventListener('load', function () {
+    dispatch(updateRoles({ roles: JSON.parse(this.responseText) }));
+  });
+  rolesReq.open('GET', 'http://localhost:3000/role');
+  rolesReq.send();
+
   return (
     <div className="wrapper">
       <HelloWorld />
